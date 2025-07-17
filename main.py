56f466685abbfeecbbf45fd645f1a206a13c253b
Virendra Vikram Singh <153907622+virendra0817@users.noreply.github.com>
2025-07-17 23:45:58 +0530

    Update main.py
    imported os

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import google.generativeai as genai
+import os
 
 app = FastAPI()
 
